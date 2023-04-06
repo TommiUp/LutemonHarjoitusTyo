3af3322984141e1461e8e10daf07038ca980f78f
EliasKukkonen <elias.v.kukkonen@gmail.com>
2023-04-06 11:53:29 +0300

    Siis luotu Lutemon activity_list, jossa on viewholder. Lutemon list activity on tehty. Lutemon list adapter on keskeneräinen.

--- app/src/main/java/com/example/lutemonapp/LutemonListActivity.java
+++ app/src/main/java/com/example/lutemonapp/LutemonListActivity.java
@@ -1,14 +1,32 @@
 package com.example.lutemonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Collections;
+
 public class LutemonListActivity extends AppCompatActivity {
+
+    private LutemonStorage storage;
+
+    private RecyclerView  recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lutemon_list);
+
+        storage = LutemonStorage.getInstance();
+
+        recyclerView = findViewById(R.id.rvLutemonList);
+
+        Collections.sort(LutemonStorage.getInstance().getLutemons(), (lutemon1, lutemon2) -> lutemon1.getName().compareTo(lutemon2.getName()));
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new LutemonListAdapter(storage.getLutemons(), getApplicationContext()));
     }
 }
--- app/src/main/java/com/example/lutemonapp/LutemonListAdapter.java
+++ app/src/main/java/com/example/lutemonapp/LutemonListAdapter.java
@@ -1,4 +1,38 @@
 package com.example.lutemonapp;
 
-public class LutemonListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LutemonListAdapter extends RecyclerView.Adapter<LutemonViewHolder> {
+    private Context context;
+
+    private ArrayList<Lutemon> lutemons = new ArrayList<>();
+
+
+
+    public LutemonListAdapter(ArrayList<Lutemon> lutemons, Context context){
+        this.lutemons = lutemons;
+        this.context = context;
+    }
+
+
+
+    @NonNull
+    @Override
+    public LutemonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new LutemonViewHolder(LayoutInflater.from(context).inflate(R.layout.lutemon_view, parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LutemonViewHolder holder, int position){
+
+    }
+
+
 }
