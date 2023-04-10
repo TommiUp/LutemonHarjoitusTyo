31ac3d41c4e7a94ee8e05733466840ccc70764e5
TommiUp <tuponen@gmail.com>
2023-04-10 11:34:55 +0300

    Todennäköisyys lisätty tappeluihin

--- app/src/main/java/com/example/lutemonapp/LutemonBattleActivity.java
+++ app/src/main/java/com/example/lutemonapp/LutemonBattleActivity.java
@@ -47,21 +47,27 @@ public class LutemonBattleActivity extends AppCompatActivity {
         }
         if (checkedLutemons.size() == 2) {
             StringBuilder battleInfo = new StringBuilder();
+            // Show stats of the lutemons before the battle
+            for (Lutemon lutemon : checkedLutemons) {
+                battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
+                        lutemon.getId(), lutemon.getName(), lutemon.attack(), lutemon.defence(), lutemon.getExperience(),
+                        lutemon.getHealth(), lutemon.getMaxHealth()));
+            }
             Lutemon attacker = checkedLutemons.get(0);
             Lutemon defender = checkedLutemons.get(1);
             boolean battleOver = false;
             while (!battleOver) {
                 // Attacker attacks defender
-                int damage = attacker.attack() - defender.defence();
+                int damage = attacker.attack() - defender.defence() + (int)(Math.random() * 4);
                 if (damage > 0) {
                     defender.takeDamage(damage);
+                    battleInfo.append(String.format("%s attacks %s\n", attacker.getName(), defender.getName()));
                     battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
                             attacker.getId(), attacker.getName(), attacker.attack(), attacker.defence(), attacker.getExperience(),
                             attacker.getHealth(), attacker.getMaxHealth()));
                     battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
                             defender.getId(), defender.getName(), defender.attack(), defender.defence(), defender.getExperience(),
                             defender.getHealth(), defender.getMaxHealth()));
-                    battleInfo.append(String.format("%s attacks %s\n", attacker.getName(), defender.getName()));
                     if (defender.getHealth() == 0) {
                         battleInfo.append(String.format("%s gets defeated.\nThe battle is over.", defender.getName()));
                         attacker.increaseVictory();
@@ -74,16 +80,16 @@ public class LutemonBattleActivity extends AppCompatActivity {
                     battleInfo.append(String.format("%s attacks %s\n", attacker.getName(), defender.getName()));
                 }
                 // Defender attacks attacker
-                damage = defender.attack() - attacker.defence();
+                damage = defender.attack() - attacker.defence() + (int)(Math.random() * 4);
                 if (damage > 0) {
                     attacker.takeDamage(damage);
-                    battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
-                            defender.getId(), defender.getName(), defender.attack(), defender.defence(), defender.getExperience(),
-                            defender.getHealth(), defender.getMaxHealth()));
+                    battleInfo.append(String.format("%s attacks %s\n", defender.getName(), attacker.getName()));
                     battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
                             attacker.getId(), attacker.getName(), attacker.attack(), attacker.defence(), attacker.getExperience(),
                             attacker.getHealth(), attacker.getMaxHealth()));
-                    battleInfo.append(String.format("%s attacks %s\n", defender.getName(), attacker.getName()));
+                    battleInfo.append(String.format("%d: %s att: %d; def: %d; exp:%d; health:%d/%d\n",
+                            defender.getId(), defender.getName(), defender.attack(), defender.defence(), defender.getExperience(),
+                            defender.getHealth(), defender.getMaxHealth()));
                     if (attacker.getHealth() == 0) {
                         battleInfo.append(String.format("%s gets defeated.\nThe battle is over.", attacker.getName()));
                         defender.increaseVictory();
