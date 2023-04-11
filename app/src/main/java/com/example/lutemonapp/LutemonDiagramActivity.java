a891e0ad5d3745a2d52befa385026afbee86a646
TommiUp <tuponen@gmail.com>
2023-04-11 11:45:44 +0300

    Kaksi diagrammia

--- app/src/main/java/com/example/lutemonapp/LutemonDiagramActivity.java
+++ app/src/main/java/com/example/lutemonapp/LutemonDiagramActivity.java
@@ -25,17 +25,23 @@ public class LutemonDiagramActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lutemon_diagram);
         storage = LutemonStorage.getInstance();
         AnyChartView anyChartView = findViewById(R.id.chart_lutemon_stat);
+        AnyChartView anyChartView2 = findViewById(R.id.chart_lutemon_stat2);
 
         Pie pie = AnyChart.pie();
+        Pie pie2 = AnyChart.pie();
 
         List<DataEntry> data = new ArrayList<>();
+        List<DataEntry> data2 = new ArrayList<>();
 
         for (Lutemon lutemon : storage.getLutemons("all")){
             data.add(new ValueDataEntry(lutemon.getName(), lutemon.getWins()));
+            data2.add(new ValueDataEntry(lutemon.getName(), lutemon.getLost()));
         }
 
         pie.data(data);
+        pie2.data(data2);
 
         anyChartView.setChart(pie);
+        anyChartView2.setChart(pie2);
     }
 }
