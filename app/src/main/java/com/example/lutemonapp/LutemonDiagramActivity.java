59f2367aa03786b79b651de7e27073d1e79340f3
TommiUp <tuponen@gmail.com>
2023-04-11 13:14:15 +0300

    Voittoprosenttia visualisoitu diagrammina.

--- app/src/main/java/com/example/lutemonapp/LutemonDiagramActivity.java
+++ app/src/main/java/com/example/lutemonapp/LutemonDiagramActivity.java
@@ -13,35 +13,23 @@ import com.anychart.charts.Pie;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class LutemonDiagramActivity extends AppCompatActivity {
-
     private LutemonStorage storage;
-
+    private List<DataEntry> data;
+    private Pie pie;
+    private AnyChartView anyChartView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lutemon_diagram);
         storage = LutemonStorage.getInstance();
-        AnyChartView anyChartView = findViewById(R.id.chart_lutemon_stat);
-        AnyChartView anyChartView2 = findViewById(R.id.chart_lutemon_stat2);
-
-        Pie pie = AnyChart.pie();
-        Pie pie2 = AnyChart.pie();
-
-        List<DataEntry> data = new ArrayList<>();
-        List<DataEntry> data2 = new ArrayList<>();
-
+        anyChartView = findViewById(R.id.chart_lutemon_stat);
+        data = new ArrayList<>();
+        pie = AnyChart.pie();
         for (Lutemon lutemon : storage.getLutemons("all")){
             data.add(new ValueDataEntry(lutemon.getName(), lutemon.getWins()));
-            data2.add(new ValueDataEntry(lutemon.getName(), lutemon.getLost()));
         }
-
         pie.data(data);
-        pie2.data(data2);
-
         anyChartView.setChart(pie);
-        anyChartView2.setChart(pie2);
     }
 }
