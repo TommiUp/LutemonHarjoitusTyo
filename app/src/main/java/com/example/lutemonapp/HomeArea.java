ef93fabaee72a2b60bdc97940a70956c1040e83c
paddington19 <113182838+paddington19@users.noreply.github.com>
2023-04-26 01:54:03 +0300

    lisätty kommentteja

--- app/src/main/java/com/example/lutemonapp/HomeArea.java
+++ app/src/main/java/com/example/lutemonapp/HomeArea.java
@@ -6,7 +6,7 @@ public class HomeArea extends LutemonStorage {
     public HomeArea() {
         super();
     }
-    // Static method for returnin the single instance of HomeArea, creating it if it does not exist
+    // Static method for returning the single instance of HomeArea, creating it if it does not exist
     public static HomeArea getInstance() {
         if (instance == null) {
             instance = new HomeArea();
