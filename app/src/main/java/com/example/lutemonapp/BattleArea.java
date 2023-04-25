e5db31bd518a2e039ba0ef5621894578940150a1
paddington19 <113182838+paddington19@users.noreply.github.com>
2023-04-26 01:51:59 +0300

    Lisätty kommentteja

--- app/src/main/java/com/example/lutemonapp/BattleArea.java
+++ app/src/main/java/com/example/lutemonapp/BattleArea.java
@@ -7,7 +7,7 @@ public class BattleArea extends LutemonStorage {
         super();
     }
     
-    // Static method for returning the single instance of BattleArea, creating it if it does not exist
+    //Static method for returning the single instance of BattleArea, creating it if it does not exist
     public static BattleArea getInstance() {
         if (instance == null) {
             instance = new BattleArea();
